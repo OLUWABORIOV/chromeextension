8d540b6c232ee52c1529669c8725a51d542acd41
OLUWABORIOV <104468419+OLUWABORIOV@users.noreply.github.com>
2023-03-28 11:47:46 -1200

    commit

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
+const deleteBtn = document.getElementById("delete-btn")
 let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 console.log(leadsFromLocalStorage)
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
+function render(leads) {
+    let listItems = ""
+for (let i = 0; i < leads.length; i++) {
+    listItems += `
+    <li>
+    <a target='_blank' href='${leads[i]}'>
+    ${leads[i]}
+    </a>
+    </li>
+    `
+}
+ulEl.innerHTML = listItems
+ 
+}   
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 
-localStorage.setItem( ('myLeads'), JSON.stringify(myLeads) )
-    renderLeads()
-    console.log( localStorage.getItem("myLeads") )
+
 })
 
- function renderLeads() {
-    let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li>
-    <a target='_blank' href='${myLeads[i]}'>
-    ${myLeads[i]}
-    </a>
-    </li>`
  
-}   
-ulEl.innerHTML =listItems
- }
+
 
 
 //localStorage.setItem("myName", "Per Harald Borgen")
